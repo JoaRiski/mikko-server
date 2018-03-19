f540b0fa96d1c57e63722e859176b086970055b3
Rauli Laine <rauli.laine@iki.fi>
2018-03-19 18:10:33 +0200

    Do not use external command for audio playing
    Get rid of the unreliable play-sound library and implement playback of
    MP3 files directly by using LAME and speaker libraries. Also cache audio
    files received from the TTS server into local file system and implement
    a simple playback queue.

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const bodyParser = require('body-parser');
 const buildUrl = require('build-url');
 const express = require('express');
+const fs = require('fs');
+const lame = require('lame');
 const md5 = require('md5');
+const os = require('os');
 const path = require('path');
+const request = require('request');
+const speaker = require('speaker');
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8080;
 const VOICE_CONFIGURATIONS = {
@@ -29,7 +34,8 @@ const VOICE_CONFIGURATIONS = {
 };
 
 const app = express();
-const player = require('play-sound')();
+const queue = [];
+let isPlaying = false;
 
 app.use(bodyParser.json());
 
@@ -51,16 +57,75 @@ app.post('/', (req, res) => {
     return;
   }
 
-  player.play(getTTSUrl(text, voiceConfig.engine, voiceConfig.voice, voiceConfig.language));
-
   res.json({ nep: true });
+
+  const hash = buildHash(text, voiceConfig);
+  const file = path.join(os.tmpdir(), `tts-${hash}.mp3`);
+
+  if (fs.existsSync(file)) {
+    addToQueue(file);
+    return;
+  }
+
+  const url = buildUrl('http://cache-a.oddcast.com', {
+    path: `c_fs/${hash}.mp3`,
+    queryParams: {
+      engine: voiceConfig.engine,
+      language: voiceConfig.language,
+      voice: voiceConfig.voice,
+      text: encodeURIComponent(text),
+      useUTF8: 1
+    }
+  });
+
+  try {
+    request(url).pipe(fs.createWriteStream(file)).on('close', () => addToQueue(file));
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 app.listen(PORT, () => {
   process.stdout.write(`Listening on port ${PORT}.\n`);
 });
 
-function buildHash(text, engine, voice, language) {
+function addToQueue(file) {
+  queue.push(file);
+  if (!isPlaying) {
+    playFromQueue();
+  }
+}
+
+function playFromQueue() {
+  if (isPlaying || !queue.length) {
+    return;
+  }
+
+  const decoder = new lame.Decoder();
+  const file = queue[0];
+
+  queue.shift();
+  fs.createReadStream(file).pipe(decoder).once('format', () => {
+    const output = new speaker({
+      channels: 1,
+      bitDepth: 16,
+      sampleRate: 22050,
+      mode: lame.MONO
+    });
+
+    isPlaying = true;
+    decoder.pipe(output);
+    output.once('close', () => {
+      isPlaying = false;
+      if (queue.length > 0) {
+        playFromQueue();
+      }
+    });
+  });
+}
+
+function buildHash(text, voiceConfig) {
+  const { engine, language, voice } = voiceConfig;
   const fragments = [
     `<engineID>${engine}</engineID>`,
     `<voiceID>${voice}</voiceID>`,
@@ -70,19 +135,4 @@ function buildHash(text, engine, voice, language) {
   ];
 
   return md5(fragments.join(''));
-}
-
-function getTTSUrl(text, engine, voice, language) {
-  const hash = buildHash(text, engine, voice, language);
-
-  return buildUrl('http://cache-a.oddcast.com', {
-    path: `c_fs/${hash}.mp3`,
-    queryParams: {
-      engine,
-      language,
-      voice,
-      text: encodeURIComponent(text),
-      useUTF8: 1
-    }
-  });
 }
